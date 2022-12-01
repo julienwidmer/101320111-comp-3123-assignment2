e1da7fe3e822457dfccd9bdbb9537492d93e204b
Julien Widmer <ju.widmer@pm.me>
2022-11-30 22:41:30 -0500

    Remove icon from navbar primary button

--- employee-management-app/src/Navbar.js
+++ employee-management-app/src/Navbar.js
@@ -3,7 +3,16 @@ import React from "react";
 function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">{props.name}</a>
+            <a className="navbar-brand fw-bold my-0" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                     className="bi bi-buildings me-2 mb-1" viewBox="0 0 16 16">
+                    <path
+                        d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022ZM6 8.694 1 10.36V15h5V8.694ZM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15Z"/>
+                    <path
+                        d="M2 11h1v1H2v-1Zm2 0h1v1H4v-1Zm-2 2h1v1H2v-1Zm2 0h1v1H4v-1Zm4-4h1v1H8V9Zm2 0h1v1h-1V9Zm-2 2h1v1H8v-1Zm2 0h1v1h-1v-1Zm2-2h1v1h-1V9Zm0 2h1v1h-1v-1ZM8 7h1v1H8V7Zm2 0h1v1h-1V7Zm2 0h1v1h-1V7ZM8 5h1v1H8V5Zm2 0h1v1h-1V5Zm2 0h1v1h-1V5Zm0-2h1v1h-1V3Z"/>
+                </svg>
+                {props.name}
+            </a>
             <button type="button" className="navbar-toggler collapsed" data-bs-toggle="collapse"
                     data-bs-target="#navbarAdvanced" aria-controls="navbarAdvanced" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -17,14 +26,7 @@ function Navbar(props) {
                     </li>
                 </ul>
                 <a href="#" className="btn btn-primary ms-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                         className="bi bi-person-fill-add me-2" viewBox="0 0 16 16">
-                        <path
-                            d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
-                        <path
-                            d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"></path>
-                    </svg>
-                    Add a New Employee
+                    New Employee
                 </a>
             </div>
         </nav>
