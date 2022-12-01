f10fb85270ce649d90cc27396c3931f37b129f27
Julien Widmer <ju.widmer@pm.me>
2022-12-01 00:08:24 -0500

    Implement employee deletion

--- employee-management-app/src/EmployeeDirectory.js
+++ employee-management-app/src/EmployeeDirectory.js
@@ -7,7 +7,7 @@ export default class EmployeeDirectory extends React.Component {
     }
 
     componentDidMount() {
-        axios.get("https://101320111-comp-3123-assignment1-5pmhna0lt-julienwidmer.vercel.app/api/emp/employees")
+        axios.get("https://101320111-comp-3123-assignment1-kywgwqq8m-julienwidmer.vercel.app/api/emp/employees")
             .then(res => {
                 const employees = res.data;
                 this.setState({employees});
@@ -17,13 +17,11 @@ export default class EmployeeDirectory extends React.Component {
     }
 
     deleteEmployee(id) {
-        // TODO: Delete Employee from mongoDB
-        // --- Method DELETE is not allowed by Access-Control-Allow-Methods.
-        axios.delete("https://101320111-comp-3123-assignment1-5pmhna0lt-julienwidmer.vercel.app/api/emp/employees?eid=" + id)
+        axios.delete("https://101320111-comp-3123-assignment1-kywgwqq8m-julienwidmer.vercel.app/api/emp/employees?eid=" + id)
             .then(res => {
-                console.log(res.data);
-            }, (error) => {
-                console.log("Error: " + error);
+                console.log( "Delete Employee: " + res.data);
+                // Reload page
+                window.location.reload();
             });
     }
 
@@ -36,25 +34,25 @@ export default class EmployeeDirectory extends React.Component {
                     <td>{employee.email}</td>
                     <td>{employee.salary}</td>
                     <td>
-                        <div className="row align-items-center px-2 flex-nowrap">
-                            <a href="#" className="btn btn-primary col m-1">
+                        <div className="">
+                            <a href="#" className="btn btn-primary m-1">
                                 View
                             </a>
-                            <a href="#" className="btn btn-secondary col m-1">
+                            <a href="#" className="btn btn-secondary m-1">
                                 Edit
                             </a>
                             {/* Button trigger modal */}
                             <button data-bs-toggle="modal" data-bs-target={"#deleteEmployeeModal" + (rowIndex + 1)}
-                                    className="btn btn-outline-danger col m-1">
+                                    className="btn btn-outline-danger m-1">
                                 Delete
                             </button>
                             {/* Modal */}
                             <div class="modal fade" id={"deleteEmployeeModal" + (rowIndex + 1)} tabindex="-1"
-                                 aria-labelledby="deleteEmployeeModalLabel" aria-hidden="true">
+                                 aria-labelledby={"deleteEmployeeModalLabel" + (rowIndex + 1)} aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Delete from the Employee
+                                            <h1 class="modal-title fs-5" id={"deleteEmployeeModalLabel" + (rowIndex + 1)}>Delete from the Employee
                                                 Directory?</h1>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                     aria-label="Close"></button>
@@ -68,7 +66,7 @@ export default class EmployeeDirectory extends React.Component {
                                             <button type="button" class="btn btn-secondary"
                                                     data-bs-dismiss="modal">Cancel
                                             </button>
-                                            <button type="button" onClick={this.deleteEmployee(employee._id)}
+                                            <button type="button" onClick={() => { this.deleteEmployee(employee._id) }}
                                                     class="btn btn-danger">Delete
                                             </button>
                                         </div>
@@ -83,26 +81,38 @@ export default class EmployeeDirectory extends React.Component {
     }
 
     render() {
-        return <>
-            <h1 className="pb-2 pt-4">Employee Directory</h1>
-            <div className="table-responsive">
-                <table className="table table-bordered">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Full Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Salary</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.employees.map((employee, index) => {
-                        return this.getEmployeeRow(employee, index)
-                    })}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    };
+        if (this.state.employees.length == 0) {
+            return (
+                <>
+                    <h1 className="pb-2 pt-4">Employee Directory</h1>
+                    <p className="text-muted">There are no employees stored in the the Employee Directory.</p>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <h1 className="pb-2 pt-4">Employee Directory</h1>
+                    <div className="table-responsive">
+                        <table className="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Full Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Salary</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.employees.map((employee, index) => {
+                                return this.getEmployeeRow(employee, index)
+                            })}
+                            </tbody>
+                        </table>
+                    </div>
+                    <p className="text-muted">There { this.state.employees.length > 1 ? "are " + this.state.employees.length + "employees" : "is 1 employee" } stored in the the Employee Directory.</p>
+                </>
+            )
+        }
+    }
 }
