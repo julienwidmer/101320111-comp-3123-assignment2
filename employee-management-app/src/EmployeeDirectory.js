0e26417839286b0cecc7ce7d1b183be3ceef63a7
Julien Widmer <ju.widmer@pm.me>
2022-12-02 17:47:13 -0500

    Refactor code
    Create EmployeeRow component from function inside EmployeeDirectory and implement email duplicate error message on edit form submission.

--- employee-management-app/src/EmployeeDirectory.js
+++ employee-management-app/src/EmployeeDirectory.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import EmployeeForm from "./EmployeeForm";
+import EmployeeRow from "./EmployeeRow";
 
 export default class EmployeeDirectory extends React.Component {
     state = {
@@ -24,180 +24,6 @@ export default class EmployeeDirectory extends React.Component {
                 // Reload page
                 window.location.reload();
             });
-    }
-
-    getEmployeeRow(employee, rowIndex) {
-        employee._id = employee._id ? employee._id : undefined;
-        employee.first_name = employee.first_name ? employee.first_name : "Unknown";
-        employee.last_name = employee.last_name ? employee.last_name : "Unknown";
-        employee.email = employee.email ? employee.email : "Unknown";
-        employee.gender = employee.gender ? employee.gender : "Unknown";
-        employee.salary = employee.salary ? employee.salary : 0;
-
-        return (
-            <>
-                <tr className="align-middle" key={rowIndex}>
-                    <th scope="row" className="px-4 py-3">{rowIndex + 1}</th>
-                    <td className="px-4 py-3 text-nowrap">{employee.first_name} {employee.last_name}</td>
-                    <td className="px-4 py-3">{employee.email}</td>
-                    <td className="px-4 py-3">{employee.gender}</td>
-                    <td className="px-4 py-3">C$ {employee.salary.toLocaleString('en-CA')}</td>
-                    <td className="px-4 py-3">
-                        {/* --- CRUD - Buttons --- */}
-                        <div className="text-nowrap">
-                            {/* CRUD - READ - Button trigger modal */}
-                            <button data-bs-toggle="modal" data-bs-target={"#viewEmployeeModal" + (rowIndex + 1)}
-                                    className="btn btn-primary">
-                                View
-                            </button>
-
-                            {/* CRUD - UPDATE - Button trigger modal */}
-                            <button data-bs-toggle="modal" data-bs-target={"#editEmployeeModal" + (rowIndex + 1)}
-                                    className="btn btn-secondary mx-2">
-                                Edit
-                            </button>
-
-                            {/* CRUD - REMOVE - Button trigger modal */}
-                            <button data-bs-toggle="modal" data-bs-target={"#deleteEmployeeModal" + (rowIndex + 1)}
-                                    className="btn btn-danger">
-                                Delete
-                            </button>
-                        </div>
-
-                        {/* --- CRUD - Modals --- */}
-                        {/* CRUD - READ - Modal */}
-                        <div className="modal fade" id={"viewEmployeeModal" + (rowIndex + 1)} tabIndex="-1"
-                             aria-labelledby={"viewEmployeeModalLabel" + (rowIndex + 1)} aria-hidden="true">
-                            <div className="modal-dialog modal-dialog-centered">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5"
-                                            id={"viewEmployeeModalLabel" + (rowIndex + 1)}>
-                                            View Employee
-                                        </h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"/>
-                                    </div>
-                                    { <EmployeeForm employee={employee} editMode={false}/> }
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* CRUD - Edit - Modal */}
-                        <div className="modal fade" id={"editEmployeeModal" + (rowIndex + 1)} tabIndex="-1"
-                             aria-labelledby={"editEmployeeModalLabel" + (rowIndex + 1)} aria-hidden="true">
-                            <div className="modal-dialog modal-dialog-centered">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5"
-                                            id={"editEmployeeModalLabel" + (rowIndex + 1)}>
-                                            Edit Employee
-                                        </h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"/>
-                                    </div>
-                                    { <EmployeeForm employee={employee} editMode={true}/> }
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* CRUD - REMOVE - Modal */}
-                        <div className="modal fade" id={"deleteEmployeeModal" + (rowIndex + 1)} tabIndex="-1"
-                             aria-labelledby={"deleteEmployeeModalLabel" + (rowIndex + 1)} aria-hidden="true">
-                            <div className="modal-dialog modal-dialog-centered">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5"
-                                            id={"deleteEmployeeModalLabel" + (rowIndex + 1)}>
-                                            Delete Employee
-                                        </h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"/>
-                                    </div>
-                                    <div className="modal-body">
-                                        <p>Do you really want to delete <span
-                                            className="fw-bold">"{employee.first_name} {employee.last_name}"</span> from
-                                            the Employee Directory?</p>
-                                    </div>
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-secondary"
-                                                data-bs-dismiss="modal">Cancel
-                                        </button>
-                                        <button type="button" onClick={() => {
-                                            this.deleteEmployee(employee._id)
-                                        }}
-                                                className="btn btn-danger">Delete
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-            </>
-        );
-    }
-
-    getEmployeeForm(employee, editMode) {
-        return (
-            <>
-                <form>
-                    <div className="modal-body">
-                        {/* First Name */}
-                        <div className="mb-3">
-                            <label htmlFor="inputFirstName" className="form-label">First Name</label>
-                            <input type="text" className="form-control"
-                                   placeholder="First Name" id="inputFirstName" disabled={ !editMode }
-                                   defaultValue={ employee.first_name }/>
-                        </div>
-
-                        {/* Last Name */}
-                        <div className="mb-3">
-                            <label htmlFor="inputLastName" className="form-label">Last Name</label>
-                            <input type="text" className="form-control"
-                                   placeholder="Last Name" id="inputLastName" disabled={ !editMode }
-                                   defaultValue={ employee.last_name }/>
-                        </div>
-
-                        {/* Email */}
-                        <div className="mb-3">
-                            <label htmlFor="inputEmail" className="form-label">Email</label>
-                            <input type="email" className="form-control"
-                                   placeholder="Email" id="inputEmail" disabled={ !editMode } defaultValue={ employee.email }/>
-                        </div>
-
-                        {/* Gender */}
-                        <div className="mb-3">
-                            <label htmlFor="inputGender" className="form-label">Gender</label>
-                            <select className="form-select" aria-label="Gender"
-                                    id="inputGender" disabled={ !editMode }>
-                                <option>- Select an option -</option>
-                                <option value="Male" selected={ employee.gender === "Male" }>Male</option>
-                                <option value="Female" selected={ employee.gender === "Female" }>Female</option>
-                                <option value="Other" selected={ employee.gender === "Other" }>Other</option>
-                            </select>
-                        </div>
-
-                        {/* Salary */}
-                        <div className="mb-3">
-                            <label htmlFor="inputSalary" className="form-label">Salary</label>
-                            <div className="input-group w-100">
-                                <span className="input-group-text" id="inputSalary">C$</span>
-                                <input type="number" className="form-control"
-                                       placeholder="Enter an amount"
-                                       aria-label="Enter an amount"
-                                       aria-describedby="inputSalary" disabled={ !editMode } defaultValue={ employee.salary }/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" hidden={ !editMode }>Cancel</button>
-                        <button type="submit" className="btn btn-primary" hidden={ !editMode }>Save Changes</button>
-                        <button type="button" className="btn btn-primary" data-bs-dismiss="modal" hidden={ editMode } >Close</button>
-                    </div>
-                </form>
-            </>
-        );
     }
 
     render() {
@@ -226,7 +52,7 @@ export default class EmployeeDirectory extends React.Component {
                             </thead>
                             <tbody className="text-start">
                             {this.state.employees.map((employee, index) => {
-                                return this.getEmployeeRow(employee, index)
+                                return <EmployeeRow employee={employee} rowIndex={index} key={employee._id}/>
                             })}
                             </tbody>
                         </table>
--- employee-management-app/src/EmployeeForm.js
+++ employee-management-app/src/EmployeeForm.js
@@ -4,10 +4,15 @@ import axios from "axios";
 export default class EmployeeForm extends React.Component {
     constructor(props){
         super(props);
-        this.setState(props.employee);
+
+        this.state = {
+            employee: props.employee,
+            editMode: props.editMode,
+            error: false
+        };
     }
 
-    handleSubmit(event, id) {
+    handleSubmit(event) {
         // Prevent page refresh
         event.preventDefault();
 
@@ -20,57 +25,68 @@ export default class EmployeeForm extends React.Component {
             salary: event.target.elements.salary.value
         }
 
-        // TODO: On cancel button reset props? from state? YESY???
-        //TODO: Display validation error message
-        // Update Employee
-        axios.put("https://101320111-comp-3123-assignment1-kywgwqq8m-julienwidmer.vercel.app/api/emp/employees/" + this.props.employee._id, newEmployeeInfo)
-            .then(res => {
-                // Reload page
+        // PUT Request
+        axios.put("https://101320111-comp-3123-assignment1-kywgwqq8m-julienwidmer.vercel.app/api/emp/employees/" + this.state.employee._id, newEmployeeInfo)
+            .then(() => {
+                // Success -> Reload page to display updated rows
                 window.location.reload();
-            });
+            }).catch(error => {
+                // Error -> Display error message
+                this.setState({
+                    employee: this.props.employee,
+                    editMode: this.props.editMode,
+                    error: true
+                });
+
+                console.log("Error: " + JSON.stringify(error.response.data));
+                this.forceUpdate();
+
+                console.log(this.state);
+        });
     }
 
     render() {
         return (
             <>
-                <form onSubmit={event => (this.handleSubmit(event, this.props.employee._id))}>
+                <form onSubmit={event => (this.handleSubmit(event, this.state.employee._id))}>
                     <div className="modal-body">
                         {/* First Name */}
                         <div className="mb-3">
                             <label htmlFor="inputFirstName" className="form-label">First Name</label>
                             <input type="text" className="form-control"
-                                   placeholder="First Name" id="inputFirstName" disabled={ !this.props.editMode }
+                                   placeholder="First Name" id="inputFirstName" disabled={ !this.state.editMode }
                                    name="firstName"
-                                   defaultValue={ this.props.employee.first_name }/>
+                                   defaultValue={ this.state.employee.first_name }/>
                         </div>
 
                         {/* Last Name */}
                         <div className="mb-3">
                             <label htmlFor="inputLastName" className="form-label">Last Name</label>
                             <input type="text" className="form-control"
-                                   placeholder="Last Name" id="inputLastName" disabled={ !this.props.editMode }
+                                   placeholder="Last Name" id="inputLastName" disabled={ !this.state.editMode }
                                    name="lastName"
-                                   defaultValue={ this.props.employee.last_name }/>
+                                   defaultValue={ this.state.employee.last_name }/>
                         </div>
 
                         {/* Email */}
                         <div className="mb-3">
                             <label htmlFor="inputEmail" className="form-label">Email</label>
                             <input type="email" className="form-control"
-                                   placeholder="Email" id="inputEmail" disabled={ !this.props.editMode }
+                                   placeholder="Email" id="inputEmail" disabled={ !this.state.editMode }
                                    name="email"
-                                   defaultValue={ this.props.employee.email }/>
+                                   defaultValue={ this.state.employee.email }/>
                         </div>
 
                         {/* Gender */}
                         <div className="mb-3">
                             <label htmlFor="inputGender" className="form-label">Gender</label>
-                            <select className="form-select" aria-label="Gender"
-                                    id="inputGender" disabled={ !this.props.editMode } name="gender">
+                            <select className="form-select"
+                                    aria-label="Gender" id="inputGender" disabled={ !this.state.editMode } name="gender"
+                                    defaultValue={ this.state.employee.gender }>
                                 <option disabled>- Select an option -</option>
-                                <option value="Male" selected={ this.props.employee.gender === "Male" }>Male</option>
-                                <option value="Female" selected={ this.props.employee.gender === "Female" }>Female</option>
-                                <option value="Other" selected={ this.props.employee.gender === "Other" }>Other</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Other">Other</option>
                             </select>
                         </div>
 
@@ -82,18 +98,22 @@ export default class EmployeeForm extends React.Component {
                                 <input type="number" step=".01" className="form-control" name="salary"
                                        placeholder="Enter an amount"
                                        aria-label="Enter an amount"
-                                       aria-describedby="inputSalary" disabled={ !this.props.editMode }
-                                       defaultValue={ this.props.employee.salary } />
+                                       aria-describedby="inputSalary" disabled={ !this.state.editMode }
+                                       defaultValue={ this.state.employee.salary } />
                             </div>
+                        </div>
+
+                        <div className="mb-3 alert alert-danger" role="alert" hidden={!this.state.error}>
+                            <span className="fw-bold">Error:</span> Changes could not be saved. Please verify that the email address is not assigned to another employee or try again later.
                         </div>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"
-                                hidden={ !this.props.editMode }>Cancel</button>
+                                hidden={ !this.state.editMode } onClick={this.closeModal}>Cancel</button>
                         <button type="submit" className="btn btn-primary"
-                                hidden={ !this.props.editMode }>Save Changes</button>
+                                hidden={ !this.state.editMode }>Save Changes</button>
                         <button type="button" className="btn btn-primary" data-bs-dismiss="modal"
-                                hidden={ this.props.editMode }>Close</button>
+                                hidden={ this.state.editMode }>Close</button>
                     </div>
                 </form>
             </>
